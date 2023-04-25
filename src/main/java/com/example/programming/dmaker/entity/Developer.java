645531688380457cd2b349e8ca4366c7b05b3927
Gwemin <dbwnghks921@naver.com>
2023-04-25 07:55:11 +0000

    Developer Entity DTO create

---
+++ src/main/java/com/example/programming/dmaker/entity/Developer.java
@@ -0,0 +1,55 @@
+package com.example.programming.dmaker.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.example.programming.dmaker.type.DeveloperLevel;
+import com.example.programming.dmaker.type.DeveloperSkillType;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Developer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @Enumerated(EnumType.STRING)
+    private DeveloperLevel developerLevel;
+
+    @Enumerated(EnumType.STRING)
+    private DeveloperSkillType developerSkillType;
+
+    private Integer experienceYears;
+    private String memberId;
+    private String name;
+    private Integer age;
+
+    @CreatedDate
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    private LocalDateTime updateAt;
+
+}
