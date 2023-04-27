2fdeef7b6c4b924ca4f861c6e6015c236baf8d7b
Gwemin <dbwnghks921@naver.com>
2023-04-27 14:42:09 +0000

    Dev SkillType enum

--- src/main/java/com/example/programming/dmaker/type/DeveloperSkillType.java
+++ src/main/java/com/example/programming/dmaker/type/DeveloperSkillType.java
@@ -6,9 +6,9 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum DeveloperSkillType {
-    BACKEND("백엔드 개발자"),
-    FRONTEND("프론트 개발자"),
-    FULLSTACK("풀스택 개발자");
+    BACK_END("백엔드 개발자"),
+    FRONT_END("프론트 개발자"),
+    FULL_STACK("풀스택 개발자");
 
     private final String description;
 }
