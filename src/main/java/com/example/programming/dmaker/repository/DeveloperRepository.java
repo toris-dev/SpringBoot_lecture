9595d1fbd6c08e1e9a5d3a3fe5378ff1194f363d
Gwemin <dbwnghks921@naver.com>
2023-04-27 14:41:40 +0000

    DevRepo JPA 이용

--- src/main/java/com/example/programming/dmaker/repository/DeveloperRepository.java
+++ src/main/java/com/example/programming/dmaker/repository/DeveloperRepository.java
@@ -1,5 +1,7 @@
 package com.example.programming.dmaker.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,5 @@ import com.example.programming.dmaker.entity.Developer;
  */
 @Repository
 public interface DeveloperRepository extends JpaRepository<Developer, Long> {
+    Optional<Developer> findByMemberId(String memberId);
 }
