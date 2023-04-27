bc479780dbb25bd3239cdbcdeafe939565f9d241
Gwemin <dbwnghks921@naver.com>
2023-04-27 14:40:38 +0000

    DevDTO add

---
+++ src/main/java/com/example/programming/dmaker/dto/DeveloperDto.java
@@ -0,0 +1,34 @@
+package com.example.programming.dmaker.dto;
+
+import com.example.programming.dmaker.entity.Developer;
+import com.example.programming.dmaker.type.DeveloperLevel;
+import com.example.programming.dmaker.type.DeveloperSkillType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class DeveloperDto {
+
+    private DeveloperLevel developerLevel;
+    private DeveloperSkillType developerSkillType;
+    private String memberId;
+
+    // 세부적이지 않은 심플한 DTO를 만들어주는 public static method 작성
+    public static DeveloperDto fromEntity(Developer developer) {
+        return DeveloperDto.builder()
+                .developerLevel(developer.getDeveloperLevel())
+                .developerSkillType(developer.getDeveloperSkillType())
+                .memberId(developer.getMemberId())
+                .build();
+
+    }
+}
