6404688c7d55c0d884d46154fc39703312c26d6a
Gwemin <dbwnghks921@naver.com>
2023-04-27 14:39:55 +0000

    get, post method add

--- src/main/java/com/example/programming/dmaker/controller/DMakerController.java
+++ src/main/java/com/example/programming/dmaker/controller/DMakerController.java
@@ -1,14 +1,19 @@
 package com.example.programming.dmaker.controller;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.programming.dmaker.dto.CreateDeveloper;
+import com.example.programming.dmaker.dto.DeveloperDetailDto;
+import com.example.programming.dmaker.dto.DeveloperDto;
 import com.example.programming.dmaker.service.DMakerService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,17 +24,24 @@ public class DMakerController {
     private final DMakerService dMakerService;
 
     @GetMapping("/developers")
-    public List<String> getAllDevelopers() {
+    public List<DeveloperDto> getAllDevelopers() {
         log.info("GET /developers HTTP/1.1");
-        return Arrays.asList("snow", "elsa", "Olaf");
+        return dMakerService.getAllDevelopers();
     }
 
-    @GetMapping("/create-developer")
-    public List<String> createDevelopers() {
-        log.info("GET /create-developer HTTP/1.1");
+    @GetMapping("/developer/{memberId}")
+    public DeveloperDetailDto getDeveloperdetail(@PathVariable String memberId) {
+        log.info("GET /developers/{memberId} HTTP/1.1");
 
-        dMakerService.createDeveloper();
+        return dMakerService.getDeveloperDetail(memberId);
+    }
 
-        return Collections.singletonList("Olaf");
+    @PostMapping("/create-developer")
+    public CreateDeveloper.Response createDevelopers(
+            @Valid @RequestBody CreateDeveloper.Request request) { // @Valid 를 사용함으로 써 메소드에 진입하기전에 메소드notException이 발생
+        log.info("request: {}", request);
+
+        return dMakerService.createDeveloper(request);
+
     }
 }
