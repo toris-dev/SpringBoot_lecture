7704313ad867edd42825e2188820b82ab8b973bf
Gwemin <dbwnghks921@naver.com>
2023-04-30 14:49:41 +0000

    Put Method 추가 (멤버아이디, 기술스택, 년차  -> body)

--- src/main/java/com/example/programming/dmaker/controller/DMakerController.java
+++ src/main/java/com/example/programming/dmaker/controller/DMakerController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.programming.dmaker.dto.CreateDeveloper;
 import com.example.programming.dmaker.dto.DeveloperDetailDto;
 import com.example.programming.dmaker.dto.DeveloperDto;
+import com.example.programming.dmaker.dto.EditDeveloper;
 import com.example.programming.dmaker.service.DMakerService;
 
 import jakarta.validation.Valid;
@@ -42,6 +44,16 @@ public class DMakerController {
         log.info("request: {}", request);
 
         return dMakerService.createDeveloper(request);
+
+    }
+
+    @PutMapping("/developer/{memberId}")
+    public DeveloperDetailDto editDeveloper(
+            @PathVariable String memberId,
+            @Valid @RequestBody EditDeveloper.Request request) { // @Valid 를 사용함으로 써 메소드에 진입하기전에 메소드notException이 발생
+        log.info("request: {}", request);
+
+        return dMakerService.editDeveloper(memberId, request);
 
     }
 }
