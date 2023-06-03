530d0bfdb42f0a2c39040c77dbb20ee81dc9895d
Gwemin <dbwnghks921@naver.com>
2023-06-03 05:01:22 +0000

    java formatting

--- src/main/java/com/example/programming/dmaker/type/DeveloperLevel.java
+++ src/main/java/com/example/programming/dmaker/type/DeveloperLevel.java
@@ -6,10 +6,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum DeveloperLevel {
-    NEW("신입 개발자"),
-    JUNIOR("주니어 개발자"),
-    JUNGNIOR("중니어 개발자"),
-    SENIOR("시니어 개발자");
+  NEW("신입 개발자"), JUNIOR("주니어 개발자"), JUNGNIOR("중니어 개발자"), SENIOR("시니어 개발자");
 
-    private final String description;
+  private final String description;
 }
--- src/main/java/com/example/programming/dmaker/type/DeveloperSkillType.java
+++ src/main/java/com/example/programming/dmaker/type/DeveloperSkillType.java
@@ -6,9 +6,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum DeveloperSkillType {
-    BACK_END("백엔드 개발자"),
-    FRONT_END("프론트 개발자"),
-    FULL_STACK("풀스택 개발자");
+  BACK_END("백엔드 개발자"), FRONT_END("프론트 개발자"), FULL_STACK("풀스택 개발자");
 
-    private final String description;
+  private final String description;
 }
